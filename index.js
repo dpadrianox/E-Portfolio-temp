d7cbab069f5b715a34b070cbc8f5ae59a2ea3810
Princess <dpadrianox@gmail.com>
2025-05-14 11:04:22 -0400

    modal overlays

---
+++ index.js
@@ -0,0 +1,20 @@
+// template_zzrgzns
+// service_pjk6a7o
+// jTE51flZyJ7Dzrp-Y
+
+function contact(event) {
+    event.preventDefault();
+    // emailjs
+    //     .sendForm(
+    //         'service_pjk6a7o',
+    //         'template_zzrgzns',
+    //         event.target,
+    //         'jTE51flZyJ7Dzrp-Y'
+    // ).then(() => {
+    //     console.log('this worked1')
+    // })
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    setTimeout(() => {
+        console.log('it worked2')}, 500)
+}
