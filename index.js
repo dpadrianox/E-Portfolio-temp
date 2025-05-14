244ca30134d11d7562ec50dfbebf23973cc92d5f
Princess <dpadrianox@gmail.com>
2025-05-14 13:10:17 -0400

    contact modal

--- index.js
+++ index.js
@@ -4,17 +4,28 @@
 
 function contact(event) {
     event.preventDefault();
-    // emailjs
-    //     .sendForm(
-    //         'service_pjk6a7o',
-    //         'template_zzrgzns',
-    //         event.target,
-    //         'jTE51flZyJ7Dzrp-Y'
-    // ).then(() => {
-    //     console.log('this worked1')
-    // })
     const loading = document.querySelector('.modal__overlay--loading');
     const success = document.querySelector('.modal__overlay--success');
-    setTimeout(() => {
-        console.log('it worked2')}, 500)
-}
+    loading.classList += " model__overlay--visible"
+
+    emailjs
+        .sendForm(
+            'service_pjk6a7o',
+            'template_zzrgzns',
+            event.target,
+            'jTE51flZyJ7Dzrp-Y'
+    ).then(() => {
+        // throw new Error('fake error');
+        loading.classList.remove("model__overlay--visible")
+        success.classList += " model__overlay--visible"
+    }).catch(() => {
+        loading.classList.remove("model__overlay--visible")
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly at dpadrianox@gmail.com."
+        )
+    })
+    // setTimeout(() => {
+        
+    //     console.log('it worked2')}, 1000)
+    }
+
