d6c2815841aa48ace2adaf2c66d9b0edd9d6f8d3
Princess <dpadrianox@gmail.com>
2025-05-15 13:57:03 -0400

    final tweaks

--- index.js
+++ index.js
@@ -4,6 +4,21 @@
 
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll('.shape');
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; i++) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+    }
+}
+
+
 
 function toggleContrast() {
     contrastToggle = !contrastToggle;
@@ -13,13 +28,13 @@ function toggleContrast() {
     else {
         document.body.classList.remove("dark-theme");
     }
-
+}
 function contact(event) {
     event.preventDefault();
     const loading = document.querySelector('.modal__overlay--loading');
     const success = document.querySelector('.modal__overlay--success');
     loading.classList += " model__overlay--visible"
-}
+
     emailjs
         .sendForm(
             'service_pjk6a7o',
